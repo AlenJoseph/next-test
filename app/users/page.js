937bcc05432139a6a2900e408b9295a0b4b02030
Alen Joseph <alenjoseph333@gmail.com>
2024-08-22 14:22:39 +0530

    initial update

---
+++ app/users/page.js
@@ -0,0 +1,14 @@
+// app/users/page.js
+
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <p>This is a static page for users.</p>
+    </div>
+  );
+};
+
+export default Users;
